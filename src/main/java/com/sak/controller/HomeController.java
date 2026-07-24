0fa94d5e55d0605e7acbf04dd919ee886a8cff52
Sai Kumar <saikumarsairam164@gmail.com>
2026-07-24 10:20:44 +0530

    done2

--- src/main/java/com/sak/controller/HomeController.java
+++ src/main/java/com/sak/controller/HomeController.java
@@ -3,69 +3,64 @@ package com.sak.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sak.entity.User;
 import com.sak.service.ProductService;
 import com.sak.service.UserService;
 
-
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-   @Autowired
-   private UserService userService;
-    
-   @Autowired
-   AdminController adminController; 
+    @Autowired
+    private UserService userService;
 
-   @Autowired
-   ProductService productService;
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/")
     public String home(Model model) {
         productService.getProductHome(model);
         return "home";
-        // return new ModelAndView("home");
     }
 
     @GetMapping("/signup")
     public ModelAndView showsignup(Model model) {
         model.addAttribute("user", new User());
         return new ModelAndView("signup");
-    }  
+    }
 
     @PostMapping("/signup")
     public ModelAndView getsignup(@ModelAttribute User user) {
-       return  userService.saveUser(user);
+        return userService.saveUser(user);
     }
 
     @GetMapping("/login")
-    public ModelAndView showlogin() {
-        return new ModelAndView("login");
+    public String showlogin() {
+        return "login";
     }
 
     @PostMapping("/login")
-public ModelAndView getlogin(@RequestParam String userName,
-                             @RequestParam String password) {
+    public String getlogin(@RequestParam String userName,
+                           @RequestParam String password,
+                           Model model) {
 
-    User auth = userService.authenticate(userName, password);
+        // Admin Login
+        if ("admin".equals(userName) && "admin".equals(password)) {
+            return "redirect:/admin/admin_dash";
+        }
 
-    if (auth != null) {
-        return new ModelAndView("home");
+        // User Login
+        User auth = userService.authenticate(userName, password);
+
+        if (auth != null) {
+            productService.getProductHome(model);
+            return "home";
+        }
+
+        model.addAttribute("error", "Invalid Username or Password");
+        return "login";
     }
-
-    if (userName.equals("admin") && password.equals("admin")) {
-        return new ModelAndView("redirect:/admin/admin_dash");
-    }
-
-    return new ModelAndView("login")
-            .addObject("error", "Invalid credentials");
-}
 }
